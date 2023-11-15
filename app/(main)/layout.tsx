3743e04df59c84acde9d34f79b2eaaabaeec6e18
SO-K-T <WsoktW@gmail.com>
2023-11-15 21:50:39 +0330

    message

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,6 +3,11 @@ import { Inter } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
+import Navbar from "@/components/shared/Navbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Footer from "@/components/shared/Footer";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,7 +23,23 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+
+        <Navbar/>
+        
+        <main>
+          <LeftSidebar/>
+
+          <section className="main-container">
+            <div className="w-full max-w-4xl">
+              {children}
+            </div>
+          </section>
+
+          <RightSidebar/>
+        </main>
+        <Footer/>
+        </body>
     </html>
     </ClerkProvider>
   );
